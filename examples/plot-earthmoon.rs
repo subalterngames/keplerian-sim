d4c4b1242aa811b6f2f418cd67e5f139e2ed1192
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-11-10 21:44:38 +0700

    Try to get plotting to work

--- examples/plot-earthmoon.rs
+++ examples/plot-earthmoon.rs
@@ -1,11 +1,16 @@
 use keplerian_rust::{
     Universe, body_presets
 };
+use egui::{self, CentralPanel, Visuals};
+use egui_plotter::EguiBackend;
 use plotters::prelude::*;
-use plotters_backend::piston::PistonBackend;
+use std::time::Duration;
 
 const SIMULATION_TICKS: u128 = 1_000_000;
-const OUTPUT_PATH: &str = "~earth_moon_orbits.png";
+const WINDOW_SIZE: (u32, u32) = (800, 600);
+
+const MOVE_SCALE: f32 = 0.01;
+const SCROLL_SCALE: f32 = 0.001;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut universe = create_earth_moon();
@@ -22,42 +27,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Done!");
     println!("Showing plot...");
 
-    // Create a window
-    let mut window: PistonWindow = WindowSettings::new("3D Orbit", [800, 600])
-    .exit_on_esc(true)
-    .build()
-    .unwrap();
+    let native_options = eframe::NativeOptions::default();
 
-    // Create a drawing area
-    let root = PistonBackend::new(&mut window).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-
-    // Create a 3D chart
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Earth-Moon Orbit", ("sans-serif", 50))
-        .build_cartesian_3d(-1.0..1.0, -1.0..1.0, -1.0..1.0)
-        .unwrap();
-
-    chart.configure_axes().draw().unwrap();
-
-    // Plot the moon positions
-    chart
-        .draw_series(LineSeries::new(
-            moon_positions.iter().map(|&(x, y, z)| (x, y, z)),
-            &RED,
-        ))
-        .unwrap()
-        .label("Moon Orbit")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-    chart.configure_series_labels().draw().unwrap();
-
-    // Render the window
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |c, g, _| {
-            root.draw(&c, g).unwrap();
-        });
-    }
+    eframe::run_native(
+        "Earth-Moon System",
+        native_options,
+        Box::new(|cc| Box::new(ChartInfo::new(cc)))
+    )?;
 
     return Ok(());
 }
@@ -74,4 +50,129 @@ fn create_earth_moon() -> Universe {
     universe.add_body(moon, Some(earth_idx));
 
     return universe;
+}
+
+struct ChartInfo {
+    chart_pitch: f32,
+    chart_yaw: f32,
+    chart_scale: f32,
+    chart_pitch_vel: f32,
+    chart_yaw_vel: f32,
+}
+
+impl ChartInfo {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Disable feathering as it causes artifacts
+        let context = &cc.egui_ctx;
+
+        context.tessellation_options_mut(|tess_options| {
+            tess_options.feathering = false;
+        });
+
+        // Also enable light mode
+        context.set_visuals(Visuals::light());
+
+        Self {
+            chart_pitch: 0.3,
+            chart_yaw: 0.9,
+            chart_scale: 0.9,
+            chart_pitch_vel: 0.0,
+            chart_yaw_vel: 0.0,
+        }
+    }
+}
+
+
+impl eframe::App for ChartInfo {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            // First, get mouse data
+            let (pitch_delta, yaw_delta, scale_delta) = ui.input(|input| {
+                let pointer = &input.pointer;
+                let delta = pointer.delta();
+
+                let (pitch_delta, yaw_delta) = match pointer.primary_down() {
+                    true => (delta.y * MOVE_SCALE, -delta.x * MOVE_SCALE),
+                    false => (self.chart_pitch_vel, self.chart_yaw_vel),
+                };
+
+                let scale_delta = input.scroll_delta.y * SCROLL_SCALE;
+
+                (pitch_delta, yaw_delta, scale_delta)
+            });
+
+            self.chart_pitch_vel = pitch_delta;
+            self.chart_yaw_vel = yaw_delta;
+
+            self.chart_pitch += self.chart_pitch_vel;
+            self.chart_yaw += self.chart_yaw_vel;
+            self.chart_scale += scale_delta;
+
+            // Next plot everything
+            let backend = EguiBackend::new(ui);
+            let root = backend.into_drawing_area();
+
+            root.fill(&WHITE).unwrap();
+
+            let x_axis = (-3.0..3.0).step(0.1);
+            let z_axis = (-3.0..3.0).step(0.1);
+
+            let mut chart = ChartBuilder::on(&root)
+                .caption(format!("3D Plot Test"), (FontFamily::SansSerif, 20))
+                .build_cartesian_3d(x_axis, -3.0..3.0, z_axis)
+                .unwrap();
+
+            chart.with_projection(|mut pb| {
+                pb.yaw = self.chart_yaw as f64;
+                pb.pitch = self.chart_pitch as f64;
+                pb.scale = self.chart_scale as f64;
+                pb.into_matrix()
+            });
+
+            chart
+                .configure_axes()
+                .light_grid_style(BLACK.mix(0.15))
+                .max_light_lines(3)
+                .draw()
+                .unwrap();
+
+            chart
+                .draw_series(
+                    SurfaceSeries::xoz(
+                        (-30..30).map(|f| f as f64 / 10.0),
+                        (-30..30).map(|f| f as f64 / 10.0),
+                        |x, z| (x * x + z * z).cos(),
+                    )
+                    .style(BLUE.mix(0.2).filled()),
+                )
+                .unwrap()
+                .label("Surface")
+                .legend(|(x, y)| {
+                    Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
+                });
+
+            chart
+                .draw_series(LineSeries::new(
+                    (-100..100)
+                        .map(|y| y as f64 / 40.0)
+                        .map(|y| ((y * 10.0).sin(), y, (y * 10.0).cos())),
+                    &BLACK,
+                ))
+                .unwrap()
+                .label("Line")
+                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLACK));
+
+            chart
+                .configure_series_labels()
+                .border_style(BLACK)
+                .draw()
+                .unwrap();
+
+            root.present().unwrap();
+        });
+
+        // Limit framerate to 100fps
+        std::thread::sleep(Duration::from_millis(10));
+        ctx.request_repaint();
+    }
 }
