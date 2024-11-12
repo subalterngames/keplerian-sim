e3a048778b4ed40a4ac9f30cb6acc7c26a7fb6d7
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-11-12 10:05:17 +0700

    Add parabolic example

---
+++ examples/dump-parabolic.rs
@@ -0,0 +1,56 @@
+use keplerian_rust::{Orbit, OrbitTrait};
+use std::{fs, path::PathBuf};
+
+const SIMULATION_TICKS: u128 = 10_000;
+const CSV_PATH: &str = "out/output-dump-parabolic.csv";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let orbit = Orbit::new(
+        1.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    );
+    let mut positions: Vec<(f64, f64, f64)> = Vec::with_capacity(SIMULATION_TICKS as usize);
+
+    print!("Simulating parabolic trajectory for {SIMULATION_TICKS} ticks...");
+
+    for i in 0..SIMULATION_TICKS {
+        let time = i as f64 / SIMULATION_TICKS as f64;
+        let pos = orbit.get_position_at_time(time);
+        positions.push(pos);
+    }
+
+    println!("Done!");
+
+    let path = PathBuf::from(CSV_PATH);
+    print!("Saving CSV to {:#?}...", &path);
+
+    fs::create_dir_all(
+        &path.parent()
+        .expect("Failed to get parent of CSV path")
+    )?;
+
+    let contents = create_csv(&positions);
+    fs::write(&path, contents)?;
+
+    println!("Done!");
+
+    return Ok(());
+}
+
+fn create_csv(positions: &Vec<(f64, f64, f64)>) -> String {
+    let mut string = String::new();
+
+    string += "time,x,y,z\n";
+
+    let iterator = positions.iter().enumerate();
+    
+    for (time, (x, y, z)) in iterator {
+        string += &format!("{time},{x},{y},{z}\n");
+    }
+
+    return string;
+}
