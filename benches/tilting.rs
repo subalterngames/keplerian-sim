d4ffc24eb1caedfb50b702100cb4b1be93ce60fd
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-11-13 22:23:04 +0700

    Refactor tilt benchmark

--- benches/tilting.rs
+++ benches/tilting.rs
@@ -5,17 +5,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 const POLL_ANGLES: usize = 1024;
 
 #[inline(always)]
-fn poll_tilt_cached(orbit: &Orbit, points: &[(f64, f64)]) {
-    for point in points {
-        black_box(orbit.tilt_flat_position(
-            black_box(point.0), 
-            black_box(point.1)
-        ));
-    }
-}
-
-#[inline(always)]
-fn poll_tilt_compact(orbit: &CompactOrbit, points: &[(f64, f64)]) {
+fn poll_tilt(orbit: &impl OrbitTrait, points: &[(f64, f64)]) {
     for point in points {
         black_box(orbit.tilt_flat_position(
             black_box(point.0), 
@@ -30,15 +20,16 @@ fn criterion_benchmark(c: &mut Criterion) {
         let angle = 2.0 * std::f64::consts::PI * (i as f64) / (POLL_ANGLES as f64);
         (angle.cos(), angle.sin())
     }).collect::<Vec<_>>();
+    let points = black_box(points);
 
     c.bench_function("tilt poll cached", |b| b.iter(||
-        poll_tilt_cached(black_box(&orbit), &points)
+        poll_tilt(black_box(&orbit), black_box(&points))
     ));
 
     let compact: CompactOrbit = orbit.into();
 
     c.bench_function("tilt poll compact", |b| b.iter(||
-        poll_tilt_compact(black_box(&compact), &points)
+        poll_tilt(black_box(&compact), black_box(&points))
     ));
 }
 
